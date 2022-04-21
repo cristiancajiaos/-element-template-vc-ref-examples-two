89b67f35660ae2367fc7bb5c48eb6ed88ff4524d
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-04-21 11:32:30 -0400

    Creado ElementRef

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
+  @ViewChild("tref", { read: ElementRef }) tref: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    console.log(this.tref.nativeElement.textContent);
   }
 
 }
